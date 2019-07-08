6c7a7f1c1b089945ef2629dca0baf1b62d36d3a1
kushal <kush.resham@yahoo.com>
2019-07-07 21:09:09 -0300

    from_master

--- app/src/main/java/com/example/android_group_project/MainActivity.java
+++ app/src/main/java/com/example/android_group_project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_group_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -38,6 +40,14 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), view_products.class);
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
