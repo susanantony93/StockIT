ba12b9d688625252a2df705d029437d660ba41cd
Susan Antony <susanantony939393@gmail.com>
2019-07-11 12:52:38 -0300

    update2

---
+++ app/src/main/java/com/example/android_group_project/Account_management.java
@@ -0,0 +1,13 @@
+package com.example.android_group_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Account_management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_management);
+    }
+}
